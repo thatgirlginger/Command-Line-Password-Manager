63e7e905f2a84ffc6a03c95150baa57025ebfd19
ging <ginger.miller@gwu.edu>
2026-02-16 14:05:54 -0500

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,17 @@
+from cryptography.fernet import Fernet
+
+def generatekey():
+    key=Fernet.generate_key()
+    with open('key.key', 'wb') as keyfile:
+        keyfile.write(key)
+
+def load_key():
+    return open('key.key', 'rb').read()
+
+def encrypt_password(password, key):
+    f = Fernet(key)
+    return f.encrypt(password.encode())
+
+def decrypt_password(encrypted_password, key):
+    f = Fernet(key)
+    return f.decrypt(encrypted_password).decode()
